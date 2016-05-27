25276c4b18313c84887bbcebfff38176bdd4156c
Steven Day <steve@mysociety.org>
2016-05-27 15:48:36 +0100

    Add inlines for Slots in the Schedule admin

--- conference/admin.py
+++ conference/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from conference import models
 
+
+class SlotInline(admin.TabularInline):
+    model = models.Slot
+    extra = 1
+
+
+admin.site.register(
+    models.Schedule,
+    prepopulated_fields={"slug": ("name",)},
+    list_display=("name",),
+    inlines = [SlotInline, ]
+)
+
 admin.site.register(models.Speaker)
-admin.site.register(models.Schedule)
 admin.site.register(models.Slot)
 admin.site.register(models.Room)
 admin.site.register(models.Presentation)
