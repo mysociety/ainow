0d43c99700ef3a6bb098f2ecf3a8cad2f8b36887
Struan Donald <struan@exo.org.uk>
2016-11-15 09:30:08 +0000

    Revert "add an /organizers url for use in top level nav and use it"
    This reverts commit 8dc6fd2ed7dda422d0c83cb7bde3e7596242ec2b.
    
    We still want to keep per workshop organisers.

--- conference/urls.py
+++ conference/urls.py
@@ -20,7 +20,6 @@ from .views import (
     SpeakerListView,
     SpeakerView,
     OrganiserTypeListView,
-    OverallOrganiserTypeListView,
     PresentationListView,
     PresentationView,
     AttendeeListView,
@@ -41,5 +40,4 @@ urlpatterns = [
     url(r'^schedule/(?P<schedule_slug>[-\w]+)/attendee/(?P<slug>[-\w]+)$', AttendeeView.as_view(), name='attendee'),
     url(r"^profile/$", AttendeeCreateUpdateView.as_view(), name="profile"),
     url(r"^profile/delete-photo$", delete_photo, name="profile_delete_photo"),
-    url(r'^organizers$', OverallOrganiserTypeListView.as_view(), name='overall-organisers'),
 ]
