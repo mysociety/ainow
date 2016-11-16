5730fb4504bbfca31b9faca49a5264bd7d97b6aa
Struan Donald <struan@exo.org.uk>
2016-11-16 18:56:18 +0000

    update the admin to use the new organiser types

--- conference/admin.py
+++ conference/admin.py
@@ -32,10 +32,17 @@ class SpeakerAdmin(SortableAdminMixin, AdminImageMixin, admin.ModelAdmin):
 admin.site.register(models.Speaker, SpeakerAdmin)
 
 
-class OrganiserAdmin(AdminImageMixin, admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+class OrganiserScheduleTypeInline(admin.TabularInline):
+    model = models.Organiser.organiser_schedule_type.through
 
-admin.site.register(models.OrganiserType)
+
+class OrganiserAdmin(SortableAdminMixin, AdminImageMixin, admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    inlines = [OrganiserScheduleTypeInline,]
+
+admin.site.register(
+    models.OrganiserType,
+)
 admin.site.register(models.Organiser, OrganiserAdmin)
 
 
