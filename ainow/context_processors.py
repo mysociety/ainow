2237892b3dbde886bc29e4afb3847f1e9a8a1f47
Struan Donald <struan@exo.org.uk>
2016-11-16 15:15:18 +0000

    sort schedules and hide private schedules

--- ainow/context_processors.py
+++ ainow/context_processors.py
@@ -19,7 +19,7 @@ def add_schedule(request):
     """Force the schedule variable to a set value for certain requests"""
     # We can't modify the account views to set this directly, so we set it here instead
     context = {
-        'schedules': Schedule.objects.all()
+        'schedules': Schedule.objects.exclude(private=True).order_by('-name')
     }
     if request.resolver_match.url_name.startswith("account"):
         context.update({'schedule': Schedule.objects.get(slug='2016')})
