8079237d5b29a77255050d391a42f837f3f34334
Struan Donald <struan@exo.org.uk>
2016-11-17 18:14:18 +0000

    shuffle resources into being separate pages

--- resources/urls.py
+++ resources/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import CategoryListView
+from .views import CategoryListView, CategoryView
 
 urlpatterns = [
+    url(r'^resources/(?P<slug>[-\w]+)$', CategoryView.as_view(), name='category'),
     url(r'^resources$', CategoryListView.as_view(), name='resources'),
 ]
