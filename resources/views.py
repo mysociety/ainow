a8d65b97f474f1c3a39d8df4e6742514499abf2b
Struan Donald <struan@exo.org.uk>
2016-11-17 18:14:18 +0000

    shuffle resources into being separate pages

--- resources/views.py
+++ resources/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from conference.models import Schedule
 
@@ -12,4 +12,15 @@ class CategoryListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(CategoryListView, self).get_context_data(**kwargs)
         context["schedule"] = Schedule.objects.get(slug="2016")
+        context["is_resources"] = True
+        return context
+
+
+class CategoryView(DetailView):
+    model = Category
+    context_object_name = 'category'
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryView, self).get_context_data(**kwargs)
+        context["is_resources"] = True
         return context
