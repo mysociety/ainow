1b23fd3571059b87cc434ff0069e137305732594
Struan Donald <struan@exo.org.uk>
2016-11-23 19:20:11 +0000

    use theme static assets

--- ainow/settings/base.py
+++ ainow/settings/base.py
@@ -174,6 +174,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, config.get('CONFERENCE_THEME'), 'web'),
     os.path.join(PROJECT_ROOT, 'web'),
 )
 
