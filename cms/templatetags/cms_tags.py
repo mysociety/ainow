d3020a99e58195147d78486427f61330b3fe8bc9
pezholio <pezholio@gmail.com>
2017-02-20 12:05:12 +0000

    Add simple autogenerated navbar

---
+++ cms/templatetags/cms_tags.py
@@ -0,0 +1,28 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    return context['request'].site.root_page
+
+def has_menu_children(page):
+    return page.get_children().live().in_menu().exists()
+
+@register.inclusion_tag('cms/tags/top_menu.html', takes_context=True)
+def top_menu(context, parent, calling_page=None):
+    menuitems = parent.get_children().live().in_menu()
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        # We don't directly check if calling_page is None since the template
+        # engine can pass an empty string to calling_page
+        # if the variable passed as calling_page does not exist.
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
