7b9f04019f95345f8c0bb8f622e192e8a0a5e731
Zarino Zappia <mail@zarino.co.uk>
2019-09-27 12:00:09 +0100

    TICTeC Local 2019 schedule
    I’ve moved the TICTeC Local schedules into their own static file,
    rather than cluttering up views.py.

---
+++ conference/static_schedules.py
@@ -0,0 +1,502 @@
+# -*- coding: utf8 -*-
+from __future__ import unicode_literals
+
+Local2018Schedule = [{
+    "kind": "other",
+    "start": "09:00",
+    "name": "Conference registration & refreshments",
+}, {
+    "kind": "talk",
+    "start": "10:00",
+    "presentations": [{
+        "title": "Welcome to TICTeC Local",
+        "speakers": [{
+            "name": "Mark Cridge",
+            "role": "Chief Executive",
+            "organisation": "mySociety",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "10:10",
+    "presentations": [{
+        "title": "Civic Tech and Local Gov: the evidence base",
+        "short_description": "The research on Civic Tech at the local level shows us what’s already working — and provides ample inspiration for new initiatives.",
+        "speakers": [{
+            "name": "Dr Rebecca Rumbul",
+            "role": "Head of Research",
+            "organisation": "mySociety",
+        }]
+    }]
+}, {
+    "kind": "keynote",
+    "start": "10:20",
+    "presentations": [{
+        "title": "Opening keynote: Fixing the plumbing",
+        "short_description": "MHCLG will share how they are helping Local Government to ‘fix the plumbing’, by putting the basics in place now — and quickly.",
+        "speakers": [{
+            "name": "Paul Maltby",
+            "role": "Chief Digital Officer",
+            "organisation": "Ministry of Housing, Communities & Local Government",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "10:40",
+    "presentations": [{
+        "title": "Introducing Public Square",
+        "short_description": "Citizen participation is often low, but the desire to get things changed is rising. Public Square will explore how to increase democratic engagement beyond elections.",
+        "speakers": [{
+            "name": "Michelle Brook",
+            "organisation": "Democratic Society",
+        }, {
+            "name": "Mel Stevens",
+            "organisation": "Democratic Society",
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "11:00",
+    "name": "Refreshment break",
+}, {
+    "kind": "talk",
+    "start": "11:20",
+    "name": "Where Civic Tech meets Local Government",
+    "short_description": "Inspiring projects that are showing results — across a broad range of policy areas.",
+    "presentations": [{
+        "title": "FixMyStreet Pro: Better street reporting for citizens and councils",
+        "speakers": [{
+            "name": "Andrea Bowes",
+            "role": "Data and Information Systems Technical Architect",
+            "organisation": "Lincolnshire County Council",
+        }]
+    }, {
+        "title": "Making Waves: Tech for Water in Sierra Leone",
+        "speakers": [{
+            "name": "Zara Rahman",
+            "role": "Research, Engagement and Communities Team Lead",
+            "organisation": "The Engine Room",
+        }]
+    }, {
+        "title": "Family Story: How technology can better support Children’s Services",
+        "speakers": [{
+            "name": "Beatrice Karol Burks and Elle Tweedy",
+            "organisation": "Futuregov",
+        }]
+    }, {
+        "title": "Council as a platform: Supporting the civics",
+        "speakers": [{
+            "name": "Sarah Drummond",
+            "role": "Co-founder and MD",
+            "organisation": "Snook",
+        }]
+    }, {
+        "title": "Revealing the hidden patterns in local democracy",
+        "speakers": [{
+            "name": "Julian Tait and Jamie Whyte",
+            "organisation": "Open Data Manchester",
+        }]
+    }, {
+        "title": "Cloud - is it just pie in the sky?",
+        "speakers": [{
+            "name": "Helen Gerling",
+            "role": "Director of Consultancy",
+            "organisation": "Shaping Cloud",
+        }]
+    }, {
+        "title": "Using tech and data to provide better support for new parents",
+        "speakers": [{
+            "name": "Tayo Medupin",
+            "role": "Innovation Director",
+            "organisation": "Shift",
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "12:40",
+    "name": "Lunch",
+}, {
+    "kind": "talk",
+    "start": "13:40",
+    "presentations": [{
+        "title": "The citizen shift",
+        "short_description": "Democracy needs a helping hand. How can we promote the role of the citizen and encourage better participation in society?",
+        "speakers": [{
+            "name": "Jon Alexander",
+            "role": "Founding Partner",
+            "organisation": "New Citizenship Project",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "14:00",
+    "presentations": [{
+        "title": "Panel discussion: Reaching the furthest first",
+        "short_description": "Decisions made at the local level can have an impact on the opportunities, living standards, health and happiness of thousands of people. We hear from panellists who are putting ethical considerations at the forefront.",
+        "speakers": [{
+            "name": "Eddie Copeland",
+            "role": "Director of Government Innovation",
+            "organisation": "Nesta",
+        }, {
+            "name": "Beatrice Karol Burks",
+            "role": "Studio Director",
+            "organisation": "Futuregov",
+        }, {
+            "name": "Dr Eloise Elliott-Taysom",
+            "role": "Product Lead",
+            "organisation": "IF",
+        }, {
+            "name": "Nick Stanhope",
+            "role": "Founder and CEO",
+            "organisation": "Shift",
+        }, {
+            "name": "Steve Skelton",
+            "role": "Strategic Head: Policy & Information Services",
+            "organisation": "Stockport Council",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "14:40",
+    "presentations": [{
+        "title": "The Consul project for citizen participation",
+        "short_description": "Consul’s participation software is used by 90 governments in 18 different countries, giving citizens a voice in decisions about their own neighbourhoods. Jose highlights experiences within Madrid City Council as well as the global impact of Consul.",
+        "speakers": [{
+            "name": "José María Becerra González",
+            "organisation": "Consul Project at Madrid City Council",
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "15:00",
+    "name": "Refreshment break",
+}, {
+    "kind": "talk",
+    "start": "15:15",
+    "presentations": [{
+        "title": "Panel discussion: Citizens or customers",
+        "short_description": "The way a council talks about its residents can reveal a lot about their ethos. Can we reshape the government-to-citizen relationship for the good of all?",
+        "speakers": [{
+            "name": "Miranda Marcus",
+            "role": "Research and Development Programme Lead",
+            "organisation": "The Open Data Institute",
+        }, {
+            "name": "Jose Maria Becerra Gonzalez",
+            "role": "Consul Project",
+            "organisation": "Madrid City Council",
+        }, {
+            "name": "Jon Alexander",
+            "role": "Founding Partner",
+            "organisation": "New Citizenship Project",
+        }, {
+            "name": "Carl Whistlecraft",
+            "role": "Head of Democracy",
+            "organisation": "Kirklees Council",
+        }, {
+            "name": "Sarah Drummond",
+            "role": "Co-founder and MD",
+            "organisation": "Snook",
+        }]
+    }]
+}, {
+    "kind": "keynote",
+    "start": "15:55",
+    "presentations": [{
+        "title": "Closing keynote: The Deal",
+        "short_description": "Wigan are trying something new: a contract between residents and the council as both sides strive for a better borough. Alison shares the lessons and impacts so far.",
+        "speakers": [{
+            "name": "Alison McKenzie-Folan",
+            "role": "Deputy CEO and Digital Transformation Lead",
+            "organisation": "Wigan Council"
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "16:15",
+    "presentations": [{
+        "title": "Panel discussion: Making it happen",
+        "short_description": "Today’s all about inspiring ideas — but how are we going to get them implemented? Hear from the decision makers about what it’ll take to put things in motion.",
+        "speakers": [{
+            "name": "Emer Coleman",
+            "role": "Digital Leader",
+            "organisation": "The Federation",
+        }, {
+            "name": "Paul Maltby",
+            "role": "CDO",
+            "organisation": "Ministry of Housing, Communities & Local Government",
+        }, {
+            "name": "Alison McKenzie-Folan",
+            "role": "Deputy CEO and Digital Transformation Lead",
+            "organisation": "Wigan Council",
+        }, {
+            "name": "Theo Blackwell",
+            "organisation": "Chief Digital Officer for London",
+        }, {
+            "name": "Phil Swan",
+            "role": "Chief Information Officer",
+            "organisation": "Greater Manchester Combined Authority",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "16:55",
+    "presentations": [{
+        "title": "Wrap up",
+        "short_description": "Summing up and reflection on the day’s events and what we’ve learned.",
+        "speakers": [{
+            "name": "Linda O’Halloran",
+            "role": "Head of Local Digital Collaboration Unit",
+            "organisation": "Ministry of Housing, Communities & Local Government",
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "17:00",
+    "name": "Finish",
+}]
+
+Local2019Schedule = [{
+    "kind": "other",
+    "start": "08:30",
+    "name": "Conference registration & refreshments",
+}, {
+    "kind": "talk",
+    "start": "09:30",
+    "room": "Living Room",
+    "presentations": [{
+        "title": "Opening words",
+        "speakers": [{
+            "name": "Mark Cridge",
+            "role": "Chief Executive",
+            "organisation": "mySociety",
+        }, {
+            "name": "Theo Blackwell",
+            "role": "Chief Digital Officer for London",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "09:40",
+    "room": "Living Room",
+    "presentations": [{
+        "title": "mySociety research: evidence and impact",
+        "speakers": [{
+            "name": "Dr Rebecca Rumbul",
+            "role": "Head of Research",
+            "organisation": "mySociety",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "09:50",
+    "room": "Living Room",
+    "presentations": [{
+        "title": "How much can digital technologies do for citizen engagement?",
+        "format": "Panel discussion.",
+        "short_description": "Citizens’ Assemblies are a modern solution — so shouldn’t digital technologies play a key part in their implementation? Speakers take an honest look at what tech can do for citizen engagement processes… and what it can’t.",
+        "speakers": [{
+            "name": "Miriam Levin",
+            "role": "Head of Community Action and Giving at the Office for Civil Society",
+            "organisation": "DCMS",
+        }, {
+            "name": "Eva O’Brien",
+            "role": "Delivery Manager",
+            "organisation": "FutureGov",
+        }, {
+            "name": "Graham Smith",
+            "role": "Professor of Politics and Director of the Centre for the Study of Democracy",
+            "organisation": "Westminster University",
+        }, {
+            "name": "Tim Hughes",
+            "role": "Director",
+            "organisation": "Involve",
+            "tbc": True,
+        }, {
+            "name": "Moderator: Louise Crow",
+            "role": "Head of Development",
+            "organisation": "mySociety",
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "10:30",
+    "name": "Refreshment break"
+}, {
+    "tracks": [{
+        "kind": "talk",
+        "start": "10:45",
+        "title": "Data changes everything: informed public services",
+        "short_description": "2 × 20 minute presentations, followed by 10 minute Q&A.",
+        "room": "Living Room",
+        "presentations": [{
+            "title": "The impact of data-enabled innovation in local public services in the UK",
+            "speakers": [{
+                "name": "James Maddison",
+                "organisation": "ODI",
+            }]
+        }, {
+            "title": "Beyond open data: how the Housing Data Coalition develops actionable, community-centered services",
+            "speakers": [{
+                "name": "Georges Clement",
+                "organisation": "JustFix",
+            }]
+        }]
+    }, {
+        "kind": "talk",
+        "start": "10:45",
+        "title": "Click to engage: creating active citizens through digital technologies",
+        "room": "Committee Room 4",
+        "short_description": "2 × 20 minute presentations, followed by 10 minute Q&A.",
+        "presentations": [{
+            "title": "Civic Tech to empower democracy and increase civic engagement: Local examples from the US",
+            "speakers": [{
+                "name": "Tammy Esteves",
+                "organisation": "Troy University",
+            }]
+        }, {
+            "title": "The impacts of Civic Tech for elections in the UK",
+            "speakers": [{
+                "name": "Joe Mitchell",
+                "organisation": "Democracy Club"
+            }]
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "11:35",
+    "name": "Time to walk between breakout rooms",
+}, {
+    "tracks": [{
+        "kind": "talk",
+        "start": "11:40",
+        "title": "Earlier actions & better connections: technology combatting social problems",
+        "short_description": "2 × 20 minute presentations, followed by 10 minute Q&A.",
+        "room": "Living Room",
+        "presentations": [{
+            "title": "Using machine learning to identify food bank dependency early",
+            "speakers": [{
+                "name": "Giselle Cory",
+                "organisation": "DataKind",
+            }]
+        }, {
+            "title": "Digital tools to connect those facing homelessness with the support they need",
+            "speakers": [{
+                "name": "Chris Hildrey",
+                "organisation": "ProxyAddress",
+            }]
+        }]
+    }, {
+        "kind": "talk",
+        "start": "11:40",
+        "title": "Showing the way: support with the digital transformation process",
+        "room": "Committee Room 4",
+        "short_description": "2 × 20 minute presentations, followed by 10 minute Q&A.",
+        "presentations": [{
+            "title": "How GOV.UK Common Platforms are helping local authorities transform services for their residents",
+            "speakers": [{
+                "name": "Robbie Aitken",
+                "organisation": "GDS",
+            }]
+        }, {
+            "title": "Creating a cross-local government user research plan",
+            "speakers": [{
+                "name": "Richard Smith",
+                "organisation": "Hackney Council",
+            }]
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "12:30",
+    "room": "Living Room",
+    "name": "Standing buffet lunch",
+}, {
+    "tracks": [{
+        "kind": "talk",
+        "start": "13:10",
+        "title": "Better foresight: Civic Tech for the urban planners",
+        "short_description": "2 × 20 minute presentations, followed by 10 minute Q&A.",
+        "room": "Living Room",
+        "presentations": [{
+            "title": "Automating environmental impact analyses to improve urban planning in New York City",
+            "speakers": [{
+                "name": "Jonathan Pichot",
+                "organisation": "NYC Planning Labs",
+            }]
+        }, {
+            "title": "Creating a live map of empty commercial spaces in London",
+            "speakers": [{
+                "name": "Nicolas Bosetti",
+                "organisation": "Centre for London",
+            }]
+        }]
+    }, {
+        "kind": "talk",
+        "start": "13:10",
+        "title": "Bringing the citizens in: Civic Tech for engagement and participation",
+        "room": "Committee Room 4",
+        "short_description": "2 × 20 minute presentations, followed by 10 minute Q&A.",
+        "presentations": [{
+            "title": "Engaging citizens in urban planning: research from examples in Europe, North America and Australia",
+            "speakers": [{
+                "name": "Ian Babelon",
+                "organisation": "Northumbria University",
+            }]
+        }, {
+            "title": "TBC",
+        # }, {
+        #     "title": "Austin's Televised Town Hall and its impact on the public policymaking process",
+        #     "speakers": [{
+        #         "name": "Dr. Larry Schooler",
+        #         "organisation": "Northwestern University",
+        #         "tbc": True,
+        #     }]
+        }]
+    }]
+}, {
+    "kind": "other",
+    "start": "14:00",
+    "name": "Time to reconvene in Living Room",
+}, {
+    "kind": "keynote",
+    "start": "14:10",
+    "room": "Living Room",
+    "presentations": [{
+        "title": "TBC",
+        "speakers": [{
+            "name": "Vidhya Alakeson",
+            "role": "CEO",
+            "organisation": "Power to Change",
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "14:30",
+    "room": "Living Room",
+    "presentations": [{
+        "title": "Climate emergency: how can Civic Tech address the world’s biggest problem?",
+        "format": "Panel discussion.",
+        "speakers": [{
+            "name": "Ellie Tulloh",
+            "role": "Service Designer",
+            "organisation": "FutureGov",
+        }, {
+            "name": "Ellie ???????",
+            "organisation": "National Lottery Community Fund",
+        }, {
+            "name": "Chris Shaw",
+            "role": "Head, Scrutiny Unit",
+            "organisation": "Committee Office at House of Commons",
+            "tbc": True,
+        }]
+    }]
+}, {
+    "kind": "talk",
+    "start": "15:20",
+    "room": "Living Room",
+    "name": "Closing remarks",
+}, {
+    "kind": "other",
+    "start": "15:30",
+    "name": "Finish",
+}]
